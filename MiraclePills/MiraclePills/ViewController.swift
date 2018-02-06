07fde3b1432b303a5361e0042f94857cb4b4a93f
Rehan Javed <rehanjavedofficial@users.noreply.github.com>
2018-02-06 22:54:00 +0500

    Miracle Pills
    Project to practice UI Design.
    Logo and Label of Product with price.

---
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Rehan Javed on 06/02/2018.
+//  Copyright © 2018 RJ APPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
