e9a7a7717a134990a5c96ab549c50bb91725fbed
Rehan Javed <rehanjaved.official@gmail.com>
2018-02-07 21:10:01 +0500

    Miracle Pills Completed
    Added Fields, Buttons and learn about UI Picker View.

--- MiraclePills/MiraclePills/ViewController.swift
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var stateBtn: UIButton!
+    @IBOutlet weak var registerBtn: UIButton!
+    
+    // States of Pakistan
+    let states:[String] = ["Punjab","KPK","Sindh","Balochistan","Gilgat Baltistan","Capital Territory","Other"]
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        statePicker.dataSource = self
+        statePicker.delegate = self
+    
     }
 
     override func didReceiveMemoryWarning() {
+    
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    
+        return 1
+    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    
+        return states.count
+    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       
+        return states[row]
+    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 
+        stateBtn.setTitle(states[row], for: UIControlState.normal)
+        registerBtn.isHidden = false;
+        stateBtn.isHidden = false;
+        statePicker.isHidden = true;
+        
+    }
+    
+    @IBAction func clickStateBtn(_ sender: Any) {
+    
+        stateBtn.isHidden = true;
+        registerBtn.isHidden = true;
+        statePicker.isHidden = false;
+    
+    }
+    
 }
 
